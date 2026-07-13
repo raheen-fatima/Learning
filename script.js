618448c546508c8056826efeca9fd011048cb7ab
raheen-fatima <yourarechuss@gmail.com>
2026-07-14 03:02:34 +0500

    scrimba promises super challange

--- script.js
+++ script.js
@@ -1,22 +1,74 @@
-function createPromise() {
+// function createPromise() {
+//     return new Promise((resolve, reject) => {
+//         const success = Math.random() > 0.5
+//         if (success) {
+//             resolve("Operation successful!")
+//         } else {
+//             reject("Operation failed.")
+//         }
+//     })
+// }
+// //  promise.all means if one fail so all fail
+// try {
+//     const promise1 = createPromise()
+//     const promise2 = createPromise()
+//     const promise3 = createPromise()
+
+//     const result = await Promise.allSettled([promise1, promise2, promise3])
+//     console.log(result)
+// } catch (err) {
+//     console.log(err)
+// }
+// createPromise()
+
+
+
+function getImagePromise(url) {
     return new Promise((resolve, reject) => {
-        const success = Math.random() > 0.5
-        if (success) {
-            resolve("Operation successful!")
-        } else {
-            reject("Operation failed.")
-        }
+        setTimeout(() => {
+            const img = new Image()
+            img.src = url
+            img.alt = 'scenic image'
+            img.addEventListener('load', () => resolve(img))
+            img.addEventListener('error', () => reject(new Error(`Failed to load image: ${url}`)))
+        }, 500)
     })
 }
-//  promise.all means if one fail so all fail
-try {
-    const promise1 = createPromise()
-    const promise2 = createPromise()
-    const promise3 = createPromise()
 
-    const result = await Promise.all([promise1, promise2, promise3])
-    console.log(result)
-} catch (err) {
-    console.log(err)
+const images = [
+    'https://scrimba.com/links/advancedjs-resources-images-scenic1',
+    'https://scrimba.com/links/advancedjs-resources-images-scenic2',
+    'https://scrimba.com/links/advancedjs-resources-images-scenic3'
+]
+
+async function preloadImages(imageUrlsArr) {
+    const imgContainer = document.getElementById('img-container')
+    const uploadContainer = document.getElementById('upload-container')
+    const promises = imageUrlsArr.map((img)=>getImagePromise(img));
+    try{
+        const res =await Promise.all(promises)
+        console.log("All images loaded successfully!")
+        console.log(res)
+        res.forEach((img)=>imgContainer.appendChild(img))
+        uploadContainer.style.display = "none"
+    }catch(err){
+        console.log(err)
+    }
+    uploadContainer.body.display = "none"
+
 }
-createPromise()
+preloadImages(images)
+/*
+Challenge:
+  1. Create an array of promises using getImagePromise.
+  2. Save the results of calling all of those promises 
+     in one go to a const 'results'.
+  3. If the promises resolve: 
+     - log "All images loaded successfully!".
+     - hide 'uploadContainer'
+     - iterate over the results and render them to imgContainer.
+  4. If the promises reject:
+     - catch and log the error.
+*/
+
+document.getElementById('submit-imgs').addEventListener('click', () => preloadImages(images))
